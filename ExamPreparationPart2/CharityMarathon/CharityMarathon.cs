dbf681a1f568897d2614fc0c189cde4bf63af495
vesopk <vesopk979@gmail.com>
2017-04-19 00:19:23 +0300

    Adding some code!

---
+++ ExamPreparationPart2/CharityMarathon/CharityMarathon.cs
@@ -0,0 +1,27 @@
+﻿namespace CharityMarathon
+{
+    using System;
+    public class CharityMarathon
+    {
+        public static void Main()
+        {
+            var marathonInDays = int.Parse(Console.ReadLine());
+            var numberOfRunners = int.Parse(Console.ReadLine());
+            var averageLaps = int.Parse(Console.ReadLine());
+
+            var trackLength = int.Parse(Console.ReadLine());
+            var trackCapacity = int.Parse(Console.ReadLine());
+            var moneyPerKilometer = double.Parse(Console.ReadLine());
+
+            var maximumRunners = trackCapacity * marathonInDays;
+            var totalRunners = Math.Min(numberOfRunners, maximumRunners);
+
+            var totalMeters = (long)totalRunners * (long)trackLength * (long)averageLaps;
+            var totalKilometers = totalMeters / 1000;
+            var money = totalKilometers * moneyPerKilometer;
+
+            Console.WriteLine($"Money raised: {money:F2}");
+
+        }
+    }
+}
---
+++ ExamPreparationPart2/LadyBugs/LadyBugs.cs
@@ -0,0 +1,81 @@
+﻿namespace LadyBugs
+{
+    using System;
+    using System.Linq;
+
+    public class LadyBugs
+    {
+        public static void Main()
+        {
+            var size = int.Parse(Console.ReadLine());
+
+            var ladyBugsIndexes = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse);
+
+            var ladybugs = new int[size];
+            foreach (var ladyBugsIndex in ladyBugsIndexes)
+            {
+                if (ladyBugsIndex < 0 || ladyBugsIndex >= size) continue;
+                ladybugs[ladyBugsIndex] = 1;
+            }
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input=="end")
+                {
+                    break;
+                }
+                var inputParams = input.Split(new[] { ' '},StringSplitOptions.RemoveEmptyEntries);
+
+                var currentIndex = int.Parse(inputParams[0]);
+                var direction = inputParams[1];
+                var flyLength = int.Parse(inputParams[2]);
+
+                if (currentIndex < 0 || currentIndex >= size)
+                {
+                    continue;
+                }
+
+                if (ladybugs[currentIndex] == 0)
+                {
+                    continue;
+                }
+
+                ladybugs[currentIndex] = 0;
+                var position = currentIndex;
+
+                while (true)
+                {
+                    if (direction == "right") 
+                    {
+                        position += flyLength;
+                    }
+
+                    else
+                    {
+                        position -= flyLength;
+                    }
+
+                    if (position < 0 || position >= size)
+                    {
+                        break;
+                    }
+
+                    if (ladybugs[position] == 1)
+                    {
+                        continue;
+                    }
+
+                    else
+                    {
+                        ladybugs[position] = 1;
+                        break;
+                    }
+
+                }
+            }
+            Console.WriteLine(string.Join(" ",ladybugs));
+        }
+    }
+}
